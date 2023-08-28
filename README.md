8ebecb443620ae5eb4aa4cc48d904bfc747f18c0
Brian Ortiz <brianleeortizramirez@gmail.com>
2023-08-28 00:52:13 -0400

    Link video en Readme saludos para el yutu

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+LINK DEL MEJOR VIDEO DEL MUNDO ES INCREIBLE COMO ESTE MUCHACHO A CREADO UNA CREACION MAS GRANDE 
+QUE EL MISMISIMO MATIAS FERNANDEZ EL 14 DE LOS BLANCOS ES UN CRACK
+https://youtu.be/O0eKjhaj_3c
+https://youtu.be/O0eKjhaj_3c
+https://youtu.be/O0eKjhaj_3c
--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,16 +1,19 @@
+
 import React, { useState } from "react";
 import Card from 'react-bootstrap/Card';
-import "./Buscadorcss.css"
+import "./Buscadorcss.css" 
 
 const Buscador = ({ personajes }) => {
     const [buscarPj, setBuscarPj] = useState("");
 
+    // Función para filtrar personajes basado en la búsqueda
     const filtrarPj = (personaje) => {
         return personaje.displayName.toLowerCase().includes(buscarPj.toLowerCase());
     };
 
     return (
         <div>
+            {/* Input de búsqueda */}
             <input
                 id="busqueda"
                 type="text"
@@ -21,7 +24,9 @@ const Buscador = ({ personajes }) => {
                     setBuscarPj(e.target.value);
                 }}
             />
+            {/* Contenedor de las tarjetas de personajes */}
             <div className="personajes-container">
+                {/* Mapea y muestra tarjetas de personajes filtrados */}
                 {personajes.filter(filtrarPj).map(personaje => (
                     <Card key={personaje.uuid} className="personaje-card">
                         <Card.Img variant="top" src={personaje.displayIcon} alt={personaje.displayName} />
